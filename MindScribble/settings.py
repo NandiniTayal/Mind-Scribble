22097e5da60659ca3b8fa695685bca83bccb77fa
Nandini Tayal <nandini.tayal@gmail.com>
2018-03-18 12:06:53 +0530

    Initial Commit

--- MindScribble/settings.py
+++ MindScribble/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
