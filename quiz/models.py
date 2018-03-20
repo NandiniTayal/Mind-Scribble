a02c6779a85102ce73f08c52dc38124ed8fb3312
Nandini Tayal <nandini.tayal@gmail.com>
2018-03-20 21:07:39 +0530

    Adding Quiz and Question Data

--- quiz/models.py
+++ quiz/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
-	choice_text = models.CharField(max_length=1)
+	choice_text = models.TextField()
 	answer_id = models.IntegerField()
 	quiz= models.ForeignKey('Quiz',on_delete=models.CASCADE,)
 
